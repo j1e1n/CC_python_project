6acbeef7a5b5bf5eeb5de64fc0d22b1df2e11e6f
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-29 16:47:16 +0000

    database added

---
+++ app/db/bucket_list.sql
@@ -0,0 +1,15 @@
+DROP TABLE places;
+DROP TABLE countries;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE places (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    visited BOOLEAN,
+    country_id INT REFERENCES countries(id)
+);
