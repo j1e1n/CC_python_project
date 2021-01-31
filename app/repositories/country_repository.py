6a3173a1f83c82407c1ca453acbae1d03a21fa21
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-31 13:18:40 +0000

    country- save(), select_all()and select(id) added

---
+++ app/repositories/country_repository.py
@@ -0,0 +1,33 @@
+from db.run_sql import run_sql
+from models.country import Country
+
+
+def save(country):
+    sql = "INSERT INTO countries (name) VALUES (%s) RETURNING *"
+    values = [country.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    country.id = id
+    return country
+
+def select_all():
+    countries = []
+    sql = "SELECT * FROM countries"
+    results = run_sql(sql)
+
+    for row in results:
+        country = Country(row['name'], row['id'])
+        countries.append(country)
+    return countries
+
+
+def select(id):
+    country = None
+    sql = "SELECT * FROM countries WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result != None:
+        country = Country(result['name'], result['id'])
+    return country
+
