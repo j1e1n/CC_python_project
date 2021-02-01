a8b525e1fdbaecd52352daa25ebfb187c3ea7dea
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-02-01 10:37:40 +0000

    console updated, checked db saves and displays new entries

--- app/console.py
+++ app/console.py
@@ -9,14 +9,16 @@ import repositories.country_repository as country_repository
 place_repository.delete_all()
 country_repository.delete_all()
 
-
 country_1 = Country("United States")
 country_repository.save(country_1)
 
 country_2 = Country("England")
 country_repository.save(country_2)
 
+place_1 = Place("RMS Queen Mary", country_1, "Former war ship with a history of accidental deaths.")
+place_repository.save(place_1)
 
-
+place_2 = Place("Ancient Ram Inn", country_2, "1100s pub, said to be the most haunted hotel in the country.")
+place_repository.save(place_2)
 
 pdb.set_trace()
