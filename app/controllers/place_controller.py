0c97320a3f3ec23db434cb9ee74112354ac1c7fe
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-02-01 10:57:21 +0000

    '/places' route added, places/index.html shows list of places

--- app/controllers/place_controller.py
+++ app/controllers/place_controller.py
@@ -6,3 +6,9 @@ places_blueprint = Blueprint("places", __name__)
 from repositories import place_repository
 from repositories import country_repository
 from models.place import Place
+
+
+@places_blueprint.route('/places')
+def places():
+    places = place_repository.select_all()
+    return render_template("places/index.html", title="Places", places=places)
